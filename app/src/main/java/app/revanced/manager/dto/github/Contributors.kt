09b681cfe1455cda3f3ec0b0542e3a1b37f8cc25
Canny <canny1913@outlook.com>
2022-10-06 20:43:09 +0300

    fix: unpushed classes

---
+++ app/src/main/java/app/revanced/manager/dto/github/Contributors.kt
@@ -0,0 +1,19 @@
+package app.revanced.manager.dto.github
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Repositories(
+    @SerialName("repositories") val repositories: List<Repository>,
+)
+@Serializable
+class Repository(
+    @SerialName("name") val name: String,
+    @SerialName("contributors") val contributors: List<Contributor>,
+)
+@Serializable
+class Contributor(
+    @SerialName("login") val username: String,
+    @SerialName("avatar_url") val avatarUrl: String,
+)
