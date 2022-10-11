851358777207eb52bff335d0e2d1c5ba058266bd
PatrickMis <24607131+PatrickMis@users.noreply.github.com>
2022-10-11 21:08:19 +0200

    build: bump dependencies, add buildToolsVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("kotlin-parcelize")
-    id("com.mikepenz.aboutlibraries.plugin") version "10.4.0"
-    kotlin("plugin.serialization") version "1.7.10"
+    id("com.mikepenz.aboutlibraries.plugin") version "10.5.0"
+    kotlin("plugin.serialization") version "1.7.20"
 }
 
 repositories {
@@ -23,6 +23,7 @@ repositories {
 android {
     namespace = "app.revanced.manager"
     compileSdk = 33
+    buildToolsVersion = "33.0.0"
 
     lint {
         abortOnError = false
@@ -71,7 +72,7 @@ android {
     }
 
     buildFeatures.compose = true
-    composeOptions.kotlinCompilerExtensionVersion = "1.3.0-rc02"
+    composeOptions.kotlinCompilerExtensionVersion = "1.3.2"
 }
 
 dependencies {
@@ -84,35 +85,35 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.7.1")
 
     // Koin
-    val koinVersion = "3.2.0"
+    val koinVersion = "3.2.2"
     implementation("io.insert-koin:koin-android:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-compose:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-workmanager:3.2.1")
+    implementation("io.insert-koin:koin-androidx-compose:3.2.1")
+    implementation("io.insert-koin:koin-androidx-workmanager:$koinVersion")
 
     // Compose
-    val composeVersion = "1.3.0-alpha03"
-    implementation("androidx.compose.ui:ui:${composeVersion}")
-    debugImplementation("androidx.compose.ui:ui-tooling:${composeVersion}")
+    val composeVersion = "1.3.0-rc01"
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.compose.material3:material3:1.0.0-rc01")
     implementation("androidx.compose.material:material-icons-extended:${composeVersion}")
 
     // Accompanist
-    val accompanistVersion = "0.26.0-alpha"
+    val accompanistVersion = "0.26.5-rc"
     implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-placeholder-material:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-drawablepainter:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-flowlayout:$accompanistVersion")
 
-    // Coil (async image loading)
-    implementation("io.coil-kt:coil-compose:2.1.0")
+    // Coil (async image loading, network image)
+    implementation("io.coil-kt:coil-compose:2.2.2")
 
     // KotlinX
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
 
     // Taxi (navigation)
     implementation("com.github.X1nto:Taxi:1.2.0")
 
-    val ktorVersion = "2.0.3"
+    val ktorVersion = "2.1.2"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-android:$ktorVersion")
     implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
@@ -122,18 +123,15 @@ dependencies {
     // ReVanced
     implementation("app.revanced:revanced-patcher:6.0.0")
 
-    // Coil for network image
-    implementation("io.coil-kt:coil-compose:2.1.0")
-
     // Signing & aligning
     implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
-    implementation("com.android.tools.build:apksig:7.2.2")
+    implementation("com.android.tools.build:apksig:7.4.0-beta02")
 
     // Licenses
-    implementation("com.mikepenz:aboutlibraries-compose:10.4.0")
+    implementation("com.mikepenz:aboutlibraries-compose:10.5.0")
 
     // ListenableFuture
-    implementation("com.google.guava:guava:31.0.1-android")
+    implementation("com.google.guava:guava:31.1-android")
     implementation("androidx.concurrent:concurrent-futures:1.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.6.4")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 buildscript {
     extra.apply {
         // Global variable for some dependencies
-        set("compose_version", "1.3.0-rc02")
-        set("ktor_version", "2.0.1")
-        set("room_version", "2.4.2")
+        set("compose_version", "1.3.0-rc01")
+        set("ktor_version", "2.1.2")
+        set("room_version", "2.7.1")
     }
     repositories {
         google()
@@ -11,10 +11,10 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.4.0-alpha10" apply false
-    id("com.android.library") version "7.4.0-alpha10" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("com.google.devtools.ksp") version "1.7.10-+" apply false
+    id("com.android.application") version "7.4.0-beta02" apply false
+    id("com.android.library") version "7.4.0-beta02" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("com.google.devtools.ksp") version "1.7.20-+" apply false
 }
 repositories {
     google()
